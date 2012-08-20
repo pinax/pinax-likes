4c6111c04055f0e75e9a1c89630f8d43d775c7d3
Patrick Altman <paltman@gmail.com>
2012-08-20 10:58:23 -0500

    Fix a couple bugs in auth backend

--- phileo/auth_backends.py
+++ phileo/auth_backends.py
@@ -7,9 +7,12 @@ class CanLikeBackend(ModelBackend):
     supports_object_permissions = True
     supports_anonymous_user = True
     
+    def is_allowed(self, obj):
+        return _allowed(
+            "%s.%s" % (obj.__class__.__module__, obj.__class__.__name__)
+        )
+    
     def has_perm(self, user, perm, obj=None):
         if perm == "phileo.can_like":
-            return _allowed(
-                "%s.%s" % (s.__class__.__module__, s.__class__.__name__)
-            )
-        return super(SitePermissionsBackend, self).has_perm(user, perm)
+            return self.is_allowed(obj)
+        return super(CanLikeBackend, self).has_perm(user, perm)

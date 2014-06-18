0e0059de94e66622c966394e3495f8263b693161
James Tauber <jtauber@jtauber.com>
2014-06-18 11:35:40 -0400

    shortcut perm if not authenticated

--- phileo/auth_backends.py
+++ phileo/auth_backends.py
@@ -6,11 +6,13 @@ from phileo.utils import _allowed
 class CanLikeBackend(ModelBackend):
     supports_object_permissions = True
     supports_anonymous_user = True
-    
+
     def is_allowed(self, obj):
         return _allowed(obj)
-    
+
     def has_perm(self, user, perm, obj=None):
+        if not user.is_authenticated():
+            return False
         if perm == "phileo.can_like":
             return self.is_allowed(obj)
         return super(CanLikeBackend, self).has_perm(user, perm)

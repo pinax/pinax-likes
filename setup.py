02d842623a5b17e62d1cbfc829b0023508cf7f25
Patrick Altman <paltman@gmail.com>
2014-10-24 18:14:07 -0500

    Support both Python 2 and 3

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "Framework :: Django",
     ],
     test_suite="runtests.runtests",

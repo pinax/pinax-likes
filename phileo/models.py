5c53f0f469f986e6ce3cc162bc517fa89a77c435
Patrick Altman <paltman@gmail.com>
2014-10-24 17:28:19 -0500

    Port model string to support Python 2/3

--- phileo/models.py
+++ phileo/models.py
@@ -1,6 +1,9 @@
+from __future__ import unicode_literals
+
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
@@ -10,6 +13,7 @@ from django.contrib.contenttypes import generic
 AUTH_USER_MODEL = getattr(settings, "AUTH_USER_MODEL", "auth.User")
 
 
+@python_2_unicode_compatible
 class Like(models.Model):
 
     sender = models.ForeignKey(AUTH_USER_MODEL, related_name="liking")
@@ -28,5 +32,5 @@ class Like(models.Model):
             ("sender", "receiver_content_type", "receiver_object_id"),
         )
 
-    def __unicode__(self):
-        return u"%s likes %s" % (self.sender, self.receiver)
+    def __str__(self):
+        return "{} likes {}".format(self.sender, self.receiver)

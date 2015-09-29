d229c1e01ca44d0afbfe49d1bd478d427285015b
Thomas Schreiber <rizumu@users.noreply.github.com>
2015-09-29 16:19:04 +0200

    Shim not required with currently supported Django versions

--- pinax/likes/models.py
+++ pinax/likes/models.py
@@ -3,12 +3,8 @@ from django.db import models
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-
-try:
-    from django.contrib.contenttypes.fields import GenericForeignKey
-except ImportError:
-    from django.contrib.contenttypes.generic import GenericForeignKey
 
 
 @python_2_unicode_compatible

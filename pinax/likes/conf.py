f3b195357da64e6869e6ea6e005dbf864c31ca2c
Thomas Schreiber <rizumu@users.noreply.github.com>
2015-09-29 16:43:29 +0200

    Remove unused load_path_attr

--- pinax/likes/conf.py
+++ pinax/likes/conf.py
@@ -1,30 +1,10 @@
 from django.conf import settings  # noqa
-from django.core.exceptions import ImproperlyConfigured
-
-try:
-    from importlib import import_module
-except ImportError:
-    from django.utils.importlib import import_module
 
 from appconf import AppConf
 from collections import defaultdict
 
 
-def load_path_attr(path):
-    i = path.rfind(".")
-    module, attr = path[:i], path[i + 1:]
-    try:
-        mod = import_module(module)
-    except ImportError as e:
-        raise ImproperlyConfigured("Error importing {0}: '{1}'".format(module, e))
-    try:
-        attr = getattr(mod, attr)
-    except AttributeError:
-        raise ImproperlyConfigured("Module '{0}' does not define a '{1}'".format(module, attr))
-    return attr
-
-
-class LikeAppConf(AppConf):
+class PinaxLikesAppConf(AppConf):
 
     DEFAULT_LIKE_CONFIG = {
         "css_class_on": "icon-heart",

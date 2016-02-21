189e0d331bf08a2f544490a0a68818868e4c0c66
arthur <arthur@wallstreetweb.net>
2016-02-21 19:02:35 +0100

    Use LoginRequiredMixin instead of method decorator

--- pinax/likes/views.py
+++ pinax/likes/views.py
@@ -1,19 +1,21 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponseForbidden, JsonResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from django.template.loader import render_to_string
-from django.utils.decorators import method_decorator
 from django.views.generic import View
 
 from pinax.likes.models import Like
 from pinax.likes.signals import object_liked, object_unliked
 from pinax.likes.utils import widget_context
 
+try:
+    from account.mixins import LoginRequiredMixin
+except ImportError:
+    from django.contrib.auth.mixins import LoginRequiredMixin
 
-@method_decorator(login_required, name='dispatch')
-class LikeToogleView(View):
+
+class LikeToogleView(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         content_type = get_object_or_404(ContentType, pk=self.kwargs.get('content_type_id'))
         obj = content_type.get_object_for_this_type(pk=self.kwargs.get('object_id'))

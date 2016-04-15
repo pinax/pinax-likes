a061565d534a993c3a057e0ceeebf734765d2088
Graham Ullrich <graham@flyingcracker.com>
2016-04-15 11:31:08 -0600

    Updating the version to 2.0.1.
    Documentation update.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="a liking app for Django",
     name="pinax-likes",
     long_description=read("README.rst"),
-    version="2.0.0",
+    version="2.0.1",
     url="http://github.com/pinax/pinax-likes/",
     license="MIT",
     packages=find_packages(),

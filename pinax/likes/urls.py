3148ef635e267f69400e4d0c6d3296ebbf16c966
KatherineMichel <kthrnmichel@gmail.com>
2017-12-11 22:37:44 -0600

    Fixing app_name

--- pinax/likes/urls.py
+++ pinax/likes/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = "likes"
+app_name = "pinax_likes"
 
 urlpatterns = [
     url(r"^like/(?P<content_type_id>\d+):(?P<object_id>\d+)/$",

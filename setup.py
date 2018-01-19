cc0b06ea59597fe4166c6305e71dbcb8fec402e2
KatherineMichel <kthrnmichel@gmail.com>
2018-01-19 16:32:51 -0600

    Updating format and removing space

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ Pinax Likes
 .. image:: https://img.shields.io/pypi/v/pinax-likes.svg
     :target: https://pypi.python.org/pypi/pinax-likes/
 
+\ 
+
 .. image:: https://img.shields.io/circleci/project/github/pinax/pinax-likes.svg
     :target: https://circleci.com/gh/pinax/pinax-likes
 .. image:: https://img.shields.io/codecov/c/github/pinax/pinax-likes.svg
@@ -23,15 +25,18 @@ Pinax Likes
 .. image:: https://img.shields.io/github/issues-pr-closed/pinax/pinax-likes.svg
     :target: https://github.com/pinax/pinax-likes/pulls?q=is%3Apr+is%3Aclosed
 
+\ 
+
 .. image:: http://slack.pinaxproject.com/badge.svg
     :target: http://slack.pinaxproject.com/
 .. image:: https://img.shields.io/badge/license-MIT-blue.svg
     :target: https://opensource.org/licenses/MIT/
     
+\ 
+
 ``pinax-likes`` is a liking app for Django, allowing users to "like" and "unlike"
 any model instance in your project. Template tags provide the ability to see who
 liked an object, what objects a user liked, and more.
-
 
 Supported Django and Python Versions
 ------------------------------------
